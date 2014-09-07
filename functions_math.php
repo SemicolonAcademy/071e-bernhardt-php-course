f94c2792a745acb740b017cf830c59cbe74337d6
sapkotabhupal <sapkotabhupal@gmail.com>
2014-09-07 07:14:59 +0000

    Datetime & Math function review. Intro to HTML forms and simple form processing example.

---
+++ functions_math.php
@@ -0,0 +1,30 @@
+<html>
+
+	<head>
+		<title>
+			Bernhardt PHP
+		</title>
+
+	<body>
+		
+		
+		<?php 
+		
+			//echo rand(0,1);			
+			echo pi();
+			
+			echo "<br/>";
+			
+			echo ceil(1.45);
+			
+			echo "<br/>";
+			
+			echo floor(1.45);
+			
+			
+		?>
+		
+	</body>
+</html>
+
+	
