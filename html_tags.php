417c644700f3311a2d010bb5654d40b1c63455ad
sapkotabhupal <sapkotabhupal@gmail.com>
2014-09-10 07:19:42 +0000

    introduction to HTML tags & CSS basics

--- html_tags.php
+++ html_tags.php
@@ -1,14 +1,105 @@
 <html>
 
 	<head>
+	
 		<title>
 			Bernhardt PHP
-		</title>
+		</title>	
 
+		<style type="text/css">
+			
+			body{
+				margin-bottom:500px;
+				
+				/*
+					background: #E29853;
+				*/
+			}
+		
+		</style>
+		
+	</head>
 		
 	<body>
+	
+	<div id="wrapper">
+	
+		<a href="http://google.com">Google</a>
 		
-				
+		<a href="https://www.youtube.com/watch?v=Y0pi14wBMpg">
+		Herau Jhamke Phuli
+		</a>
+		
+		<a href="form_reg.php">
+			Register Here
+		</a>
+		
+		<br/>
+		
+		<a href="logo.jpg">
+			
+			<img src="logo.jpg" width="100" height="100"/>
+			
+		</a>
+		
+		
+		
+		
+		<h1>This is h1</h1>
+		<h2>This is h2</h2>
+		<h3>This is h3</h3>
+		<h4>This is h4</h4>
+		<h5>This is h5</h5>
+		<h6>This is h6</h6>
+		
+		<span>this is span tag</span>
+		<span>this is span tag 2</span>
+		
+		<p class="para">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+			sed do eiusmod tempor incididunt ut labore 
+			et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+			esse cillum dolore eu fugiat nulla pariatur. 
+			Excepteur sint occaecat 
+			cupidatat non proident, sunt in culpa qui 
+			officia deserunt mollit anim id est laborum
+		</p>
+		
+		<p class="para">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+			sed do eiusmod tempor incididunt ut labore 
+			et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+			esse cillum dolore eu fugiat nulla pariatur. 
+			Excepteur sint occaecat 
+			cupidatat non proident, sunt in culpa qui 
+			officia deserunt mollit anim id est laborum
+		</p>
+		
+		
+		<div> this is first div</div>
+		<div> this is second div</div>
+		
+		<img src="logo.jpg" width="100" height="100"/>
+		
+		<ul id="nav">
+			<li>
+				<a href="php.net">PHP</a> 			
+			</li>			
+			<li>Java</li>			
+			<li>C++</li>
+			<li>C#</li>
+		</ul>
+		
+		<ol>
+			<li>PHP </li>
+			<li>Java</li>
+			<li>C++</li>
+			<li>C#</li>
+		</ol>
+		
+		</div> <!--   end of wrapper div  -->
 		
 	</body>
 </html>
